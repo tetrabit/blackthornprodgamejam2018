f0c6538525b213db412bf059311cc75a32e664ad
flyingantman <flyingantman@gmail.com>
2018-10-19 00:46:33 -0400

    working check for if pixel game was done right

--- Blackthornprodgamejam2018/Assets/Game/Scripts/PixelMatch/PixelGrid.cs
+++ Blackthornprodgamejam2018/Assets/Game/Scripts/PixelMatch/PixelGrid.cs
@@ -12,6 +12,7 @@ public class PixelGrid : MonoBehaviour
 
     public int gridX, gridY;
 
+    [System.Serializable]
     public struct Pixel
     {
         public int colorNum;
@@ -27,20 +28,47 @@ public class PixelGrid : MonoBehaviour
 
     public enum Colors
     {
-        Black,
-        Blue,
-        Green,
         Grey,
-        Orange,
-        Purple,
-        Red,
+        DarkBlue,
+        LightBlue,
+        DarkGreen,
+        LightGreen,
+        DarkRed,
+        LightRed,
+        Pink,
+        Yellow,
         White,
-        Yellow
+        Black
     }
 
     Colors color = Colors.Grey;
     Colors chosenColor = Colors.Black;
+    
+    public enum SpriteName
+    {
+        Naut,
+        Eye,
+        EyeBall
+    }
 
+    public SpriteName currentSprite = SpriteName.Naut;
+
+    [System.Serializable]
+    public struct DrawSprite
+    {
+        public int x;
+        public int y;
+        public SpriteName sn;
+        public Sprite sprite;
+        public Pixel[,] pixel;
+    }
+
+    public int[,] nautCheck  = { { 0,0,4,4,4,0,0,0,0,0,0,0 }, {0,0,0,0,4,0,4,4,4,0,0,0}, {4,4,4,4,3,4,4,8,4,0,0,3}, {0,0,4,3,4,4,4,8,4,3,3,3}, {4,4,3,4,4,4,4,8,4,0,0,3}, {0,0,0,0,4,0,4,8,4,0,0,8}, {0,0,4,4,4,0,0,0,0,0,0,0} };
+    public int[,] eyeCheck = { { 0,0,0,7,9,9,9,9,0,0,0 }, { 0,0,9,9,5,5,5,9,7,0,0 }, { 0, 9, 9, 5, 6, 6, 6, 5, 9, 9, 0 }, { 9, 9, 5, 6, 6, 10, 6, 6, 5, 9, 9 }, { 9, 9, 5, 6, 10, 10, 10, 6, 5, 9, 9 }, { 9, 9, 5, 6, 6, 10, 6, 6, 5, 9, 9 }, { 9, 7, 9, 5, 6, 6, 6, 5, 9, 7, 9 }, { 7, 9, 9, 9, 5, 5, 5, 9, 9, 9, 7 }, { 0, 9, 9, 9, 9, 9, 9, 7, 9, 9, 0 }, { 0, 0, 9, 9, 9, 9, 7, 9, 9, 0, 0 }, { 0, 0, 0, 9, 9, 7, 9, 9, 0, 0, 0 } };
+    public int[,] eyeBallCheck = { { 0,0,0,7,9,9,9,9,0,0,0 }, { 0,0,9,9,1,1,1,9,7,0,0}, { 0,9,9,1,2,2,2,1,9,9,0}, {9,9,1,2,2,10,2,2,1,9,9}, {9,9,1,2,10,10,10,2,1,9,9}, {9,9,1,2,2,10,2,2,1,9,9}, {9,7,9,1,2,2,2,1,9,7,9}, {7,9,9,9,1,1,1,9,9,9,7}, {0,9,9,9,9,9,9,7,9,9,0}, {0,0,9,9,9,9,7,9,9,0,0}, {0,0,0,9,9,7,9,9,0,0,0} };
+
+    public DrawSprite[] drawSprite;
+    
     private RaycastHit rayHit;
 
     int amountOfColors;
@@ -78,14 +106,20 @@ public class PixelGrid : MonoBehaviour
     public void StartGame()
     {
         Init();
-        runUpdate = true;
     }
 
     private void Init()
     {
-        SpawnPixelGrid();
+        for(int i = 0; i < drawSprite.Length; i++)
+        {
+            if(drawSprite[i].sn == currentSprite)
+            {
+                SpawnPixelGrid(drawSprite[i].x, drawSprite[i].y);
+            }
+        }
         SpawnColorPicker();
         Resize();
+        runUpdate = true;
     }
 
     public void SpawnPixelGrid()
@@ -94,11 +128,12 @@ public class PixelGrid : MonoBehaviour
         InitGrid();
     }
 
-    public void SpawnPixelGrid(float x, float y)
+    public void SpawnPixelGrid(int x, int y)
     {
+        gridX = x;
+        gridY = y;
         pixels = new Pixel[gridX, gridY];
-        pixelGridOrigin.position = new Vector3(x, y, 0);
-        InitGrid();
+        InitGrid(gridX, gridY);
     }
 
     public void SpawnColorPicker()
@@ -112,6 +147,21 @@ public class PixelGrid : MonoBehaviour
         for (int i = 0; i < gridX; i++)
         {
             for (int j = 0; j < gridY; j++)
+            {
+                pixels[i, j].go = Instantiate(pixelCube, new Vector3(i + 0.5f, j + 0.5f, 0) + pixelGridOrigin.position, Quaternion.Euler(0, 0, 0), pixelGridOrigin);
+                pixels[i, j].go.tag = "PixelCube";
+                pixels[i, j].pc = pixels[i, j].go.GetComponent<PixelCube>();
+                pixels[i, j].colorNum = (int)color;
+                pixels[i, j].mat = GetColor(Colors.Grey);
+            }
+        }
+    }
+
+    private void InitGrid(int x, int y)
+    {
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
             {
                 pixels[i, j].go = Instantiate(pixelCube, new Vector3(i + 0.5f, j + 0.5f, 0) + pixelGridOrigin.position, Quaternion.Euler(0, 0, 0), pixelGridOrigin);
                 pixels[i, j].go.tag = "PixelCube";
@@ -153,6 +203,25 @@ public class PixelGrid : MonoBehaviour
                 }
             }
         }
+
+        //use this if you need to hard code another sprite
+        /*
+        string copyPaste = "";
+        for (int i = 0; i < gridX; i++)
+        {
+            copyPaste += "{";
+            for (int j = 0; j < gridY; j++)
+            {
+                if (j != gridY)
+                    copyPaste += pixels[i, j].colorNum + ",";
+                else
+                    copyPaste += pixels[i, j].colorNum;
+            }
+            copyPaste += "}, ";
+        }
+
+        Debug.Log(copyPaste);
+        */
     }
 
     private void PickColor(GameObject cube)
@@ -174,6 +243,51 @@ public class PixelGrid : MonoBehaviour
     private Material GetColor(Colors color)
     {
         return (Material)AssetDatabase.LoadAssetAtPath("Assets/Game/Materials/PixelCubeColors/" + color + ".mat", typeof(Material));
+    }
+
+    public void CheckDrawing()
+    {
+        bool correct = false;
+
+        for (int i = 0; i < gridX; i++)
+        {
+            for (int j = 0; j < gridY; j++)
+            {
+                if(currentSprite == SpriteName.Naut)
+                {
+                    if(pixels[i,j].colorNum == nautCheck[i,j])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        correct = false;
+                    }
+                }
+                if(currentSprite == SpriteName.Eye)
+                {
+                    if (pixels[i, j].colorNum == eyeCheck[i, j])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        correct = false;
+                    }
+                }
+                if(currentSprite == SpriteName.EyeBall)
+                {
+                    if (pixels[i, j].colorNum == eyeBallCheck[i, j])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        correct = false;
+                    }
+                }
+            }
+        }
     }
 
     IEnumerator ChangeColorOnInit(Pixel pix, Material mat)
